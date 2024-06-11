713bf0230c9c4c7c94265d7b4e740abb88c5bb2c
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-06-11 15:39:52 +0300

    First commit - lexer

---
+++ Frontend/Compiler.cs
@@ -0,0 +1,17 @@
+﻿using Frontend.Parsing.Entities;
+
+namespace Frontend
+{
+	public interface ITextProvider
+	{
+		string GetText(ILocation location);
+	}
+
+	public class Compiler : ITextProvider
+	{
+		public string GetText(ILocation location)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Frontend/Parsing/Entities/Token.cs
@@ -0,0 +1,120 @@
+﻿namespace Frontend.Parsing.Entities
+{
+	public class Token
+	{
+		public TokenType Type { get; set; }
+		public TokenLocation Location { get; set; }
+		public object Data { get; set; }
+
+		public override string ToString()
+		{
+			return $"({Location.Line}:{Location.Index - Location.LineStartIndex}) ({Type}) {Data}";
+		}
+	}
+
+	public enum TokenType : short
+	{
+		Unknown,
+
+		NewLine,
+		EOF,
+
+		DocComment,
+
+		StringLiteral,
+		CharLiteral,
+		NumberLiteral,
+
+		Identifier,
+		DollarIdentifier,
+		SharpIdentifier,
+		AtSignIdentifier,
+
+		Semicolon, // ;
+		Colon, // :
+		Comma, // ,
+		Period, // .
+		PeriodPeriod, // ..
+		Equal, // =
+		Ampersand, // &
+		Hat, // ^
+		Bang, // !
+		VerticalSlash,  // |
+
+		Plus,
+		Minus,
+		Asterisk,
+		ForwardSlash,
+		Percent,
+
+		LessLess,  // <<
+		GreaterGreater,  // >>
+
+		AddEq,
+		SubEq,
+		MulEq,
+		DivEq,
+		ModEq,
+
+		Less,
+		LessEqual,
+		Greater,
+		GreaterEqual,
+		DoubleEqual,
+		NotEqual,
+
+		LogicalOr,  // ||
+		LogicalAnd,  // &&
+
+		Arrow,  // =>
+
+		OpenParen,  // (
+		CloseParen,  // )
+		OpenBrace,  // {
+		CloseBrace,  // }
+		OpenBracket,  // [
+		CloseBracket,  // ]
+
+		// words
+		KwStruct,
+		KwEnum,
+		KwInterface,
+		KwClass,
+
+		KwIf,
+		KwElse,
+		KwSwitch,
+		KwCase,
+		KwLoop,
+
+		KwTrue,
+		KwFalse,
+		KwNull,
+		KwVoid,
+
+		KwAttach,
+		KwUsing,
+
+		KwBreak,
+		KwContinue,
+		KwReturn,
+
+		KwConst,
+		KwDefault,
+		KwNew,
+
+		KwIn,
+		KwIs,
+		KwAs,
+
+		KwPublic,
+		KwProtected,
+		KwPrivate,
+
+		KwStatic,
+		KwAbstract,
+		KwVirtual,
+		KwOverride,
+		KwPartial,
+	}
+}
