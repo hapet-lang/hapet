e0532ced6e11c6caab1aecfdaa1c40a47697a306
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2024-10-20 16:14:08 +0300

    While loop ast added

---
+++ HapetFrontend/Ast/Statements/AstWhileStmt.cs
@@ -0,0 +1,25 @@
+﻿using HapetFrontend.Ast.Expressions;
+using HapetFrontend.Types;
+
+namespace HapetFrontend.Ast.Statements
+{
+    public class AstWhileStmt : AstStatement
+    {
+        /// <summary>
+		/// The condition param of 'while' loop. Could be pure <see cref="AstExpression"/>.
+		/// Has to return <see cref="BoolType"/>
+		/// </summary>
+		public AstExpression ConditionParam { get; set; } 
+
+        /// <summary>
+        /// The body of for loop
+        /// </summary>
+        public AstBlockExpr Body { get; set; }
+
+        public AstWhileStmt(AstExpression condition, AstBlockExpr body, ILocation location = null) : base(location)
+        {
+            ConditionParam = condition;
+            Body = body;
+        }
+    }
+}
