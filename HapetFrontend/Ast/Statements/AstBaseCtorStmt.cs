8fdb7580122cb93ff2dd2770bcf8c249bb91a39a
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-01-09 12:04:27 +0300

    Base ctor stmt added

---
+++ HapetFrontend/Ast/Statements/AstBaseCtorStmt.cs
@@ -0,0 +1,18 @@
+﻿using HapetFrontend.Ast.Expressions;
+
+namespace HapetFrontend.Ast.Statements
+{
+    public class AstBaseCtorStmt : AstStatement
+    {
+        /// <summary>
+		/// The arguments to be passed into base ctor
+		/// </summary>
+		public List<AstArgumentExpr> Arguments { get; set; }
+
+        public AstBaseCtorStmt(List<AstArgumentExpr> arguments = null, ILocation Location = null)
+            : base(Location)
+        {
+            this.Arguments = arguments ?? new List<AstArgumentExpr>();
+        }
+    }
+}
