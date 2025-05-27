a813c383c2f9a6db0f1d07cbb7963efb7b092cca
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-05-27 18:24:35 +0300

    Some shite to prepare genDecl added #50

--- HapetPostPrepare/Inference/Helpers/ConstrainInferenceHelper.cs
+++ HapetPostPrepare/Inference/Helpers/ConstrainInferenceHelper.cs
@@ -1,12 +1,59 @@
-﻿using HapetFrontend.Ast.Declarations;
+﻿using System.Runtime;
+using HapetFrontend.Ast;
+using HapetFrontend.Ast.Declarations;
+using HapetFrontend.Ast.Statements;
+using HapetFrontend.Enums;
+using HapetFrontend.Types;
+using HapetPostPrepare.Entities;
 
 namespace HapetPostPrepare
 {
     public partial class PostPrepare
     {
-        public void PostPrepareGenericDeclConstrains(AstGenericDecl decl)
+        internal void PostPrepareGenericDeclConstrains(AstGenericDecl decl, InInfo inInfo, ref OutInfo outInfo)
         {
-            // TODO:
+            // handle constrains
+            foreach (var constrain in decl.Constrains)
+            {
+                switch (constrain.ConstrainType)
+                {
+                    case GenericConstrainType.CustomType:
+                        HandleCustomConstrainType(decl, constrain, inInfo, ref outInfo);
+                        break;
+                    // ...
+                }
+            }
+        }
+
+        private void HandleCustomConstrainType(AstGenericDecl decl, AstConstrainStmt constrain, InInfo inInfo, ref OutInfo outInfo)
+        {
+            // inference the ident
+            PostPrepareExprInference(constrain.Expr, inInfo, ref outInfo);
+            // custom constains are always interface/class
+            var theClass = (constrain.Expr.OutType as ClassType).Declaration;
+
+            // we need to copy all the class decls to our genericDecl
+            foreach (var d in theClass.Declarations)
+            {
+                // do not copy ini/ctor/stor/dtor funcs
+                if (d is AstFuncDecl funcDecl && (
+                    funcDecl.ClassFunctionType == ClassFunctionType.Initializer ||
+                    funcDecl.ClassFunctionType == ClassFunctionType.Ctor ||
+                    funcDecl.ClassFunctionType == ClassFunctionType.StaticCtor ||
+                    funcDecl.ClassFunctionType == ClassFunctionType.Dtor))
+                    continue;
+
+                // create a copy of the decl 
+                var copied = d.GetDeepCopy() as AstDeclaration;
+                // clear bodies/initializers - there is no need for them
+                if (copied is AstFuncDecl f && f.Body != null)
+                    f.Body.Statements.Clear();
+                else if (copied is AstVarDecl v)
+                    v.Initializer = null;
+
+                // add the copy
+                decl.Declarations.Add(copied);
+            }
         }
     }
 }
--- HapetPostPrepare/Metadata/Steps/Step2_Generics.cs
+++ HapetPostPrepare/Metadata/Steps/Step2_Generics.cs
@@ -5,7 +5,9 @@ using HapetFrontend.Ast.Statements;
 using HapetFrontend.Entities;
 using HapetFrontend.Helpers;
 using HapetFrontend.Parsing;
+using HapetFrontend.Scoping;
 using HapetFrontend.Types;
+using HapetPostPrepare.Entities;
 
 namespace HapetPostPrepare
 {
@@ -24,6 +26,10 @@ namespace HapetPostPrepare
             if (decl.IsImplOfGeneric)
                 return false;
 
+            // just handlers
+            InInfo inInfo = InInfo.Default;
+            OutInfo outInfo = OutInfo.Default;
+
             // getting pure generics from decl
             var pureGenerics = GenericsHelper.GetGenericsFromName(decl.Name as AstIdGenericExpr, _compiler.MessageHandler);
             // we need to set types to ids
@@ -40,11 +46,13 @@ namespace HapetPostPrepare
                 {
                     Constrains = currContrains,
                     ParentDecl = decl.IsImplOfGeneric ? decl.OriginalGenericDecl : decl,
-                    IsNestedDecl = true // for what? but let it be :)
+                    IsNestedDecl = true, // for what? but let it be :)
+                    SubScope = new Scope($"{currGeneric.Name}_scope", decl.Scope),
+                    SourceFile = decl.SourceFile,
                 };
 
                 // post prepare
-                PostPrepareGenericDeclConstrains(genericDecl);
+                PostPrepareGenericDeclConstrains(genericDecl, inInfo, ref outInfo);
 
                 originalGeneric.OutType = genericDecl.Type.OutType;
                 currGeneric.OutType = originalGeneric.OutType;
