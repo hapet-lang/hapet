4ed5f0b4519532fea5bd7ea7ee8133875e62998f
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-07-02 13:11:02 +0300

    SAT intrinsics ast added #94

---
+++ HapetFrontend/Ast/Expressions/AstSATOfExpr.cs
@@ -0,0 +1,35 @@
+﻿namespace HapetFrontend.Ast.Expressions
+{
+    /// <summary>
+    /// Handle for sizeof, alignof, typeof intrinsics
+    /// </summary>
+    public class AstSATOfExpr : AstExpression
+    {
+        public enum SATType
+        {
+            Sizeof,
+            Alignof,
+            Typeof,
+        }
+
+        public SATType ExprType { get; set; }
+
+        public AstSATOfExpr(SATType exprType, ILocation location = null) : base(location)
+        {
+            ExprType = exprType;
+        }
+
+        public override AstStatement GetDeepCopy()
+        {
+            var copy = new AstSATOfExpr(ExprType, Location)
+            {
+                IsCompileTimeValue = IsCompileTimeValue,
+                OutType = OutType,
+                OutValue = OutValue,
+                Scope = Scope,
+                SourceFile = SourceFile,
+            };
+            return copy;
+        }
+    }
+}
