496b857c835d813333485216966376cd05add79a
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-07-15 10:44:55 +0300

    Default defines added #48

--- HapetCompiler/ProjectConf/DefaultDefinesSetter.cs
+++ HapetCompiler/ProjectConf/DefaultDefinesSetter.cs
@@ -1,10 +1,19 @@
-﻿namespace HapetCompiler.ProjectConf
+﻿using HapetFrontend;
+using System.Reflection;
+
+namespace HapetCompiler.ProjectConf
 {
     internal partial class ProjectXmlParser
     {
         private void SetDefaultDefines()
         {
-            //_projectData.Defines.Add();
+            Assembly assem = Assembly.GetEntryAssembly();
+            AssemblyName assemName = assem.GetName();
+            string ver = assemName.Version.ToString(3);
+            _projectData.Defines.Add("HAPET_VERSION", ver);
+
+            _projectData.Defines.Add("TARGET_PLATFORM", _projectSettings.TargetPlatformData.Name);
+            _projectData.Defines.Add("CURRENT_PLATFORM", CompilerSettings.CurrentPlatformData.Name);
         }
     }
 }
