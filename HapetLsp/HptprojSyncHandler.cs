65693697b39b5cfaee32eeea6d09303643c14c56
DocHuva <85249309+dochuva222@users.noreply.github.com>
2025-09-24 10:03:32 +0300

    LSP project added #116
    Co-Authored-By: Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>

---
+++ HapetLsp/HptprojSyncHandler.cs
@@ -0,0 +1,95 @@
+﻿using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+
+namespace HapetLsp
+{
+    class HptprojSyncHandler : ITextDocumentSyncHandler
+    {
+        private readonly ILanguageServer _router;
+
+        private readonly TextDocumentSelector _documentSelector = new TextDocumentSelector(
+            new TextDocumentFilter()
+            {
+                Pattern = "**/*.hptproj"
+            }
+        );
+
+        public HptprojSyncHandler(ILanguageServer router)
+        {
+            _router = router;
+        }        
+
+        public TextDocumentAttributes GetTextDocumentAttributes(DocumentUri uri)
+        {
+            return new TextDocumentAttributes(uri, "xml");
+        }
+
+        public Task<Unit> Handle(DidChangeTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            //var documentPath = request.TextDocument.Uri.ToString();
+            //var text = request.ContentChanges.FirstOrDefault()?.Text;
+
+            //_bufferManager.UpdateBuffer(documentPath, new StringBuffer(text));
+
+            //_router.Window.LogInfo($"Updated buffer for document: {documentPath}\n{text}");
+
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(DidOpenTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            //_bufferManager.UpdateBuffer(request.TextDocument.Uri.ToString(), new StringBuffer(request.TextDocument.Text));
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(DidCloseTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Unit> Handle(DidSaveTextDocumentParams request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        TextDocumentChangeRegistrationOptions IRegistration<TextDocumentChangeRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentChangeRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                SyncKind = TextDocumentSyncKind.Incremental
+            };
+        }
+
+        TextDocumentOpenRegistrationOptions IRegistration<TextDocumentOpenRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentOpenRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+        TextDocumentCloseRegistrationOptions IRegistration<TextDocumentCloseRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentCloseRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+            };
+        }
+
+        TextDocumentSaveRegistrationOptions IRegistration<TextDocumentSaveRegistrationOptions, TextSynchronizationCapability>.GetRegistrationOptions(TextSynchronizationCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new TextDocumentSaveRegistrationOptions()
+            {
+                DocumentSelector = _documentSelector,
+                IncludeText = false,
+            };
+        }
+    }
+}
