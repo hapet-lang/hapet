7a6635a7f8c94ab10a8a554aac66065fdfac8338
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-12-29 11:01:24 +0300

    More lambda handling #100

--- HapetLastPrepare/Parts/HandleLambdas.cs
+++ HapetLastPrepare/Parts/HandleLambdas.cs
@@ -1,5 +1,9 @@
 ﻿using HapetFrontend.Ast;
 using HapetFrontend.Ast.Declarations;
+using HapetFrontend.Ast.Expressions;
+using HapetFrontend.Extensions;
+using HapetFrontend.Helpers;
+using HapetFrontend.Parsing;
 
 namespace HapetLastPrepare
 {
@@ -13,14 +17,25 @@ namespace HapetLastPrepare
             Dictionary<AstFuncDecl, List<AstStatement>> sorted = new Dictionary<AstFuncDecl, List<AstStatement>>();
             foreach (var d in _compiler.LambdasAndNested)
             {
+                // skip static lambdas and nested
+                if ((d is AstLambdaExpr l && l.SpecialKeys.Contains(TokenType.KwStatic)) ||
+                    (d is AstFuncDecl f && f.SpecialKeys.Contains(TokenType.KwStatic)))
+                    continue;
+
                 // get parent function that contains the lambda or nested
                 var parentFunc = d.FindContainingFunction();
+
+                // skip pure generics
+                if (GenericsHelper.ShouldTheDeclBeSkippedFromCodeGen(parentFunc))
+                    continue;
 
                 if (sorted.TryGetValue(parentFunc, out List<AstStatement> value))
                     value.Add(d);
                 else
                     sorted[parentFunc] = new List<AstStatement>() { d };
             }
+
+            int currentSyntheticClass = 0;
             foreach (var (parentFunc, decls) in sorted)
             {
                 // search for used outer vars in lambda
@@ -50,7 +65,23 @@ namespace HapetLastPrepare
                         // create a function to add to a new class
                         functionsToGenerate.Add(lambdaExpr.CreateFuncDecl($"Lambda{currentLambda}"));
                     }
+                    currentLambda++;
                 }
+
+                // creating a new class
+                List<AstDeclaration> classDecls = new List<AstDeclaration>();
+                classDecls.AddRange(usedDecls.Select(x => x.GetDeepCopy() as AstDeclaration));
+                classDecls.AddRange(functionsToGenerate);
+                AstClassDecl sytheticClass = new AstClassDecl(new AstIdExpr($"__SyntheticClass{currentSyntheticClass}", parentFunc.Location), 
+                    classDecls, location: parentFunc.Location);
+
+                // prepare new class
+                _postPreparer.SetScopeAndParent(sytheticClass, parentFunc.ContainingParent);
+                _postPreparer.PostPrepareDeclScoping(sytheticClass);
+                // pp up to the current metadata step
+                _postPreparer.PostPrepareStatementUpToCurrentStep(false, sytheticClass);
+
+                currentSyntheticClass++;
             }
         }
     }
