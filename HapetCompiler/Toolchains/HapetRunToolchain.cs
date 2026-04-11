8a788d85683f5ef28f62b97ccedb55b2030e7886
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-04-11 20:52:32 +0300

    Build and run help command update

--- HapetCompiler/Toolchains/HapetRunToolchain.cs
+++ HapetCompiler/Toolchains/HapetRunToolchain.cs
@@ -55,6 +55,13 @@ namespace HapetCompiler.Toolchains
         private void PrintHelp(IMessageHandler messageHandler)
         {
             messageHandler.ReportMessage($"Usage: \n  hapet run <project> <args> \n");
+            messageHandler.ReportMessage($"Parameters: ");
+            messageHandler.ReportMessage($"  -t|--target <PLATFORM> \t The platform for which the project will be built.");
+            messageHandler.ReportMessage($"\t\t\t\t If the target platform is not specified, the project will be built for the current platform.");
+            messageHandler.ReportMessage($"\t\t\t\t List of available platforms: win-x64, win-x86, linux-x64, linux-x86.");
+            messageHandler.ReportMessage($"  --verbose \t\t\t Detailed output while building.");
+            messageHandler.ReportMessage($"  --debug|--release \t\t Project build configuration.");
+            messageHandler.ReportMessage($"\t\t\t\t If no configuration is specified, --debug is used.");
         }
     }
 }
